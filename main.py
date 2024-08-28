60e31d18a72e02a906dc897636eb93784eae09bd
Cipher <tonycypher0@gmail.com>
2024-08-28 21:02:59 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from router import blog_get, blog_post
+from db import models
+from db.database import engine
+# uvicorn main:app --reload
+
+app = FastAPI()
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
+
+@app.get('/')
+def index():
+    return {'message': 'Hello World!!'}
+
+
+models.Base.metadata.create_all(engine)
---
+++ router/blog_get.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI,status, Response, APIRouter, Depends
+from router.blog_post import required_functionality
+from typing import Optional
+from enum import Enum
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blog']
+)
+
+
+@router.get(
+        '/all', 
+        summary='Retrieve all blogs',
+        description='This api call simulates fetching all blogs.',
+        response_description='The list of available blogs')
+def get_all_blogs(page=1, page_size:Optional[int] = None, req_parameter: dict=Depends(required_functionality)):
+    return {'message': f'All {page_size} blogs on page {page}', 'req': req_parameter}
+
+@router.get('/{id}/comments/{comment_id}', tags=['comment'])
+def get_comment(id:int, comment_id:int, valid:bool=True, username:Optional[str]=None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+class BlogType(str, Enum):
+    short= 'short'
+    story= 'story'
+    howto= 'howto'
+
+@router.get('/type/{type}')
+def get_blog_type(type: BlogType):
+    return {'message': f'Blog type {type}'}
+
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found.'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f'Blog with id {id}'}
