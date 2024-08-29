ffc84553f11d5aa83091d6c9cf60eb17c5f0f0b8
Cipher <tonycypher0@gmail.com>
2024-08-29 19:10:12 +0100

    Custom Response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import JSONResponse, PlainTextResponse
-from router import blog_get, blog_post, user, article
+from router import blog_get, blog_post, user, article, product
 from db import models
 from db.database import engine
 from exceptions import StoryException
@@ -11,6 +11,7 @@ app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 app.include_router(article.router)
+app.include_router(product.router)
 
 
 @app.get('/')
---
+++ router/product.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse, Response, PlainTextResponse
+
+router = APIRouter(
+    prefix='/product',
+    tags=['product']
+)
+
+products = ['watch', 'laptop', 'phone']
+
+@router.get('/all')
+def get_all_products():
+    data = " ".join(products)
+    return Response(content=data, media_type="text/plain")
+
+@router.get('/{id}', responses={
+    200: {
+        "content": {
+            "text/html": {
+                "example": "<div>Product</div>"
+            }
+        },
+        "description": "Returns the HTML for an object"
+    },
+    400: {
+        "content": {
+            "text/plain": {
+                "example": "Product not available"
+            }
+        },
+        "description": "A cleartext error message"
+    }
+})
+def get_product(id: int):
+    if id >= len(products):
+        out = "product not available"
+        return PlainTextResponse(status_code=404, content=out, media_type='text/plain')
+    else:
+        product = products[id]
+        out = f"""
+        <head>
+            <style>
+            .product {{
+                width: 500px;
+                height: 30px;
+                border: 2px insert green;
+                background-color: lightblue;
+                text-align: center;
+            }}
+            </style>
+        </head>
+        <div class="product">{product}</div>
+        """
+        return HTMLResponse(content=out, media_type="text/html")
