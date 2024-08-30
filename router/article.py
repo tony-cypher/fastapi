ca2b7c89ab18072c2e55d2a8d39df66118e0b17d
Cipher <tonycypher0@gmail.com>
2024-08-30 16:00:21 +0100

    auth by user

--- router/article.py
+++ router/article.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
-from schema import ArticleBase, ArticleDisplay
+from schema import ArticleBase, ArticleDisplay, UserBase
 from sqlalchemy.orm.session import Session
 from db import db_article
 from db.database import get_db
-from auth.oauth2 import oauth2_scheme
+from auth.oauth2 import oauth2_scheme, get_current_user
 
 router = APIRouter(
     prefix='/article',
@@ -12,10 +12,13 @@ router = APIRouter(
 
 # create article
 @router.post('/', response_model=ArticleDisplay)
-def create_article(request: ArticleBase, db: Session=Depends(get_db)):
+def create_article(request: ArticleBase, db: Session=Depends(get_db), current_user: UserBase=Depends(get_current_user)):
     return db_article.create_article(db, request)
 
 # get article
-@router.get('/{id}', response_model=ArticleDisplay)
-def get_article(id: int, db: Session=Depends(get_db), token: str=Depends(oauth2_scheme)):
-    return db_article.get_article(db, id)
+@router.get('/{id}')
+def get_article(id: int, db: Session=Depends(get_db), current_user: UserBase=Depends(get_current_user)):
+    return {
+        'data': db_article.get_article(db, id),
+        'current_user': current_user
+    }
