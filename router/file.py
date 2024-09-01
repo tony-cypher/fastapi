f530badda8c9bd104c85bbd48a63fa52775b8d89
Cipher <tonycypher0@gmail.com>
2024-09-01 23:22:36 +0100

    files: upload

--- router/file.py
+++ router/file.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, UploadFile
+import shutil
 
 
 router = APIRouter(
@@ -11,3 +12,13 @@ def get_file(file: bytes=File(...)):
     content = file.decode('utf-8')
     lines = content.split('\n')
     return {'lines': lines}
+
+@router.post('/uploadfile')
+def get_upload_file(upload_file: UploadFile=File(...)):
+    path = f"media/{upload_file.filename}"
+    with open(path, 'w+b') as buffer:
+        shutil.copyfileobj(upload_file, buffer)
+    return {
+        'filename': path,
+        'type': upload_file.content_type
+    }
