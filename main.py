49eb73d3fd51ebce0e8a1201fc9aa2651b3b9cd3
Cipher <tonycypher0@gmail.com>
2024-09-01 05:36:00 +0100

    files: basic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import JSONResponse, PlainTextResponse
 # from fastapi.middleware.cors import CORSMiddleware
-from router import blog_get, blog_post, user, article, product
+from router import blog_get, blog_post, user, article, product, file
 from auth import authentication
 from db import models
 from db.database import engine
@@ -10,6 +10,7 @@ from exceptions import StoryException
 
 app = FastAPI()
 app.include_router(authentication.router)
+app.include_router(file.router)
 app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
